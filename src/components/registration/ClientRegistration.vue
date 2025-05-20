157d585fb1408de58eae39189d28e5b1fa183062
ronpehka <ronpehka@gmail.com>
2025-05-20 11:08:13 +0300

    login wip 5

---
+++ src/components/registration/ClientRegistration.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container text-center justify-content-center">
+    <div class="row justify-content-center">
+      <div class="col col-3">
+        <div class="col col-2 input-group mb-3">
+          <label class="form-label">Eesnimi</label>
+          <input type="text" class="form-control" placeholder="Eesnimi">
+        </div>
+        <div class="input-group mb-3">
+          <label class="form-label">Perekonnanimi</label>
+          <input type="text" class="form-control" placeholder="Perekonnanimi">
+        </div>
+        <div class="input-group mb-3">
+          <label class="form-label">Email</label>
+          <input type="email" class="form-control" placeholder="email">
+        </div>
+        <div class="input-group mb-3">
+          <label class="form-label">Sünnikuupäev</label>
+          <input type="date" class="form-control" placeholder="date">
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+          <label class="form-check-label" for="flexRadioDefault1">
+            Mees
+          </label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+          <label class="form-check-label" for="flexRadioDefault2">
+            Naine
+          </label>
+        </div>
+
+        <div class="input-group mb-3">
+          <label class="form-label">Parool</label>
+          <input type="password" class="form-control" placeholder="Parool">
+        </div>
+        <div class="input-group mb-3">
+          <label class="form-label">Parool uuesti</label>
+          <input type="password" class="form-control" placeholder="Parool">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ClientRegistration'
+}
+</script>
--- src/views/RegistrationView.vue
+++ src/views/RegistrationView.vue
@@ -1,28 +1,19 @@
 
 <template>
-  <div class="container text-center justify-content-center">
-    <div class="row">
-      <div class="col">
-        <div class="input-group mb-3">
-          <span class="input-group-text"></span>
-          <input type="text" class="form-control" placeholder="Eesnimi" aria-label="Email" aria-describedby="basic-addon1">
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col">
-        <div class="input-group mb-3">
-          <span class="input-group-text"></span>
-          <input type="text" class="form-control" placeholder="Perekonnanimi" aria-label="FirsName" aria-describedby="basic-addon1">
-        </div>
-      </div>
-    </div>
+  <div>
+    <h1>Loo uus konto</h1>
+    <ClientRegistration></ClientRegistration>
+    <button type="button" class="btn btn-outline-secondary">Loo konto</button>
   </div>
+
 
 </template>
 
 <script>
+import ClientRegistration from "@/components/registration/ClientRegistration.vue";
+
 export default {
-  name: 'RegistrationView'
+  name: 'RegistrationView',
+  components: {ClientRegistration}
 }
 </script>
