b39b665e792103102b7d855941549390c6b0a9f8
Uku-Mats <profile3.confluence@kool.bcskoolitus.ee>
2025-05-28 09:54:45 +0300

    COMMIT trainingtable 27.05.

--- src/views/TrainingInfoView.vue
+++ src/views/TrainingInfoView.vue
@@ -66,7 +66,7 @@ export default {
     },
 
     getAllSports() {
-      sportService.sendGetSportsInfoRequest()
+      sportService.sendGetSportsRequest()
           .then(response => {
             this.sportInfos = response.data;
           })
