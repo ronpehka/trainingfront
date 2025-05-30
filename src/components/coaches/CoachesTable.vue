79eb1c3999eb1d91f9b44d1b542774978a150e39
uvema <uvemand@gmail.com>
2025-05-30 13:25:05 +0300

    coach OK

--- src/components/coaches/CoachesTable.vue
+++ src/components/coaches/CoachesTable.vue
@@ -18,10 +18,16 @@
         Spordialad
       </div>
     </div>
-    <!--    kõik ülejöönud read dynaamiliselt coaches pealt, row for loopi-->
+
     <div v-for="coach in coaches" :key="coach.coachUserId" class="row justify-content-center">
       <div class="col col-2">
-        <img :src="coach.imageData" alt="Treeneri pilt" class="img-fluid rounded" />
+
+        <img v-if="  coach.imageData === null || coach.imageData.length === 0" width="150" height="150"
+             src="https://media.istockphoto.com/id/1934800957/vector/man-empty-avatar-vector-photo-placeholder-for-social-networks-resumes-forums-and-dating.jpg?s=1024x1024&w=is&k=20&c=EjRPg7oLiAQ9s60K5bdvvz6TGWWS-sUVTOoesVvn9rE="
+             class="img-thumbnail" alt="Treeneri pilt">
+        <img v-else width="150" height="150" :src="coach.imageData" class="img-thumbnail" alt="Treeneri pilt">
+
+
       </div>
       <div class="col col-2">
         {{coach.coachFullName}}
